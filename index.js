934dedc98795490f786487f7b583331a689f0845
Eduardo Perez <eapv28@hotmail.com>
2025-07-23 12:38:11 -0400

    SecuredVault

---
+++ index.js
@@ -0,0 +1,23 @@
+const seven = 7;
+const three = 3;
+const eightyTwo = 82;
+const two = 2;
+const fifteen = 15;
+const four = 4;
+const one = 1;
+const thirtySix = 36;
+
+let code1 
+code1 = seven + three;
+
+let code2
+code2 = eightyTwo / two - one;
+
+let code3
+code3 = thirtySix + (fifteen % four);
+
+let message = "The vault has been secured . the combination is :" ;
+ let codeA = code1 + '-' + code2 + '-' + code3;
+ let codeB = `${code1} - ${code2} - ${code3}`;
+
+ console.log(message, codeA, codeB);
